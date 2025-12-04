4bbf1e9900d404939c4f0e7e2982d1b7c15470ce
John Phelan <johnlphelan2004@gmail.com>
2025-12-04 14:04:11 -0600

    testing basic pms reading

--- firmware/xu4Mqtt/pms7003.py
+++ firmware/xu4Mqtt/pms7003.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 baudRate = 9600
-portIn = "/dev/ttyACM0" #probably needs to be dynamically found? maybe?
+portIn = "/dev/ttyS0" #probably needs to be dynamically found? maybe?
 
 ser = serial.Serial(
     port= portIn,\
@@ -14,23 +14,20 @@ ser = serial.Serial(
 
 print(f"PMS7003 connected to {ser.port} at {ser.baudrate} baud.")
 
-def main():
-    line = []
+def main()
+    data = read_pms7003()
+
+
+def read_pms7003():
     while True:
-        try:
-            for c in ser.read():
-                line.append(chr(c))
-                if chr(c) == '\n':
-                    if len(line) < 32:
-                        continue
-                    print("".join(line))
-                    line = []
-        except KeyboardInterrupt:
-            print("Exiting...")
-            break
-        except Exception as e:
-            print("Error:", e)
-            break
+        if ser.read(1) == b'\x42':
+            if ser.read(1) == b'\x4d':
+                print("got here")
+                frame = ser.read(30)  
+                if len(frame) == 30:
+                    data = b'\x42\x4d' + frame
+                    return data
+
 
 if __name__ == "__main__":
     print("MINTS")
